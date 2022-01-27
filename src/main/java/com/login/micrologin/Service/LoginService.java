7aa4b1af88eaf9a71c4f4cd7790cc8683437f21f
Lindwing <rui.lopesgouveia@heig-vd.ch>
2022-01-27 14:37:33 +0100

    enlever import inutilisé

--- src/main/java/com/login/micrologin/Service/LoginService.java
+++ src/main/java/com/login/micrologin/Service/LoginService.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.*;
 import java.nio.charset.Charset;
-import java.time.Instant;
 import java.util.Date;
 
 public class LoginService {
